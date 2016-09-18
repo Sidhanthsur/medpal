618ec7daf0bd4f193eba5f5e0f7f35e394b03380
sidhanthsurana <sidhanth.surana12@gmail.com>
2016-09-18 13:14:09 +0530

    sidhanth

---
+++ app/src/main/java/herotalkies/vsecomm/com/vijayhouse/sidhanth.java
@@ -0,0 +1,14 @@
+package herotalkies.vsecomm.com.vijayhouse;
+
+import android.util.Log;
+
+/**
+ * Created by sidhanth on 18/9/16.
+ */
+public class sidhanth {
+
+    public void sidhanth()
+    {
+        Log.e("Whats","up ?");
+    }
+}
